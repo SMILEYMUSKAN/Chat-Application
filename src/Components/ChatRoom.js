08e8152b494825839d865baca3937b7f3041a01d
Sk.Muskan Fathima <124171813+SMILEYMUSKAN@users.noreply.github.com>
2023-10-18 16:46:38 +0000

    ChatRoom File Modified

--- src/Components/ChatRoom.js
+++ src/Components/ChatRoom.js
@@ -48,78 +48,8 @@ var chatRoom = () => {
             Start Chat
           </button>
         )}
-        <h1 className="ChatRoomHeaderUI">
-          Create Your Room
-        </h1>
-        {inputToggle ? null : (
-          <input
-            type="text"
-            value={name}
-            placeholder="Your Name"
-            onChange={HandleOnChangeName}
-            className="ChatRoomInputUI"
-          />
-        )}
-
-        {inputToggle ? (
-          <input
-            type="text"
-            value={room}
-            placeholder="Your Room Name"
-            onChange={HandleOnChange}
-            onClick={() => setInputToggle(false)}
-            className="ChatRoomInputUIToggle"
-          />
-        ) : (
-          <input
-            type="text"
-            value={room}
-            placeholder="Your Room Name"
-            onChange={HandleOnChange}
-            onClick={() => setInputToggle(false)}
-            className="ChatRoomInputUI"
-          />
-        )}
-
-        {room && name && (
-          <button
-            type="submit"
-            className="ChatRoomButtonUI"
-          >
-            Start Chat
-          </button>
-        )}
-
-        <p className="text-center">or</p>
-        <p className="ChatRoomParaUI">
-          If u already created a room then enter your room name here
-        </p>
-        {inputToggle ? (
-          <input
-            type="text"
-            value={room}
-            onChange={HandleOnChange}
-            className="ChatRoomInputUI"
-            onClick={() => setInputToggle(true)}
-          />
-        ) : (
-          <input
-            type="text"
-            value={room}
-            onChange={HandleOnChange}
-            className="ChatRoomInputUIToggle"
-            onClick={() => setInputToggle(true)}
-          />
-        )}
-
-        {inputToggle && (
-          <button
-            type="submit"
-            className="ChatRoomButtonUI"
-          >
-            Continue
-          </button>
-        )}
+        
+       
       </form>
     </div>
   );
